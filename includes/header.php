82bcfe40e918c2662b1dfc5abc67abac0e3e7741
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-27 09:20:46 +0000

    Removal of content not relevant to an internal applications.

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,8 @@
         <a href="http://discovery.nationalarchives.gov.uk/details/rd/N13980137#page_wrap">Skip to Main Content</a>
     </div>
 
-    <div id="retro-disco"><span>This is a wireframe prototype representing only a single route through a potential application process</span></div>
+    <div id="retro-disco"><span>This is a wireframe prototype representing only a single route through a potential application process</span>
+    </div>
 
     <div class="container">
         <div id="holds-logo-and-search" class="row">
@@ -35,38 +36,8 @@
                     </div>
                 </form>
                 <span id="search-expander" style="position: absolute; right: 5px;">&nbsp;</span></div>
-            <div class="col starts-at-full ends-at-half clr">
-                <div id="search-field-wrapper" role="search">
-                    <form id="globalSearch" method="get"
-                          action="http://apps.nationalarchives.gov.uk/search/quick_search.aspx">
-                        <span id="scope-selector" class="interactive-element expanded">&nbsp;</span>
-                        <input type="text" aria-required="true" required="" name="search_text" id="tnaSearch"
-                               placeholder="Search our website..." class="search-field" autocomplete="off">
-                        <input type="submit" value="" id="search-button" class="search-button">
-                    </form>
-                    <ul id="search-options" style="display: block;">
-                        <li><a aria-label="Change form destination to search the website" role="button"
-                               data-fieldname="search_text" data-placeholder="Search our website..."
-                               data-target="http://apps.nationalarchives.gov.uk/search/quick_search.aspx"
-                               class="formDestinationChanger"
-                               href="http://discovery.nationalarchives.gov.uk/details/rd/N13980137#">Search our
-                                website</a></li>
-                        <li><a aria-label="Change form destination to search the catalogue" role="button"
-                               data-fieldname="_q" data-placeholder="Search our records..." data-target="/results/r"
-                               class="formDestinationChanger"
-                               href="http://discovery.nationalarchives.gov.uk/details/rd/N13980137#">Search our
-                                records</a></li>
-                    </ul>
-                </div>
-            </div>
         </div>
-        <div class="row">
-            <div class="col starts-at-full clr pull-down">
-                <div id="mega-menu-pull-down" class="interactive-element">
-                    <button aria-label="Toggle menu">Menu</button>
-                </div>
-            </div>
-        </div>
+
     </div>
     <!-- This is where the mega-menu would be included -->
     <div id="account-controls-wrapper">
