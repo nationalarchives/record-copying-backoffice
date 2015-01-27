bf4f0f2c13433534846c54328c7c648a92e40dbb
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-27 16:35:22 +0000

    Addition of simple (static, for the time being) navigation bar

--- includes/header.php
+++ includes/header.php
@@ -12,47 +12,22 @@
                 <a title="Go to The National Archives homepage" href="http://www.nationalarchives.gov.uk/">
                     <img src="images/logo-white.png" alt="The National Archives" title="The National Archives">
                 </a>
-
-                <form id="mobile-global-search" method="get"
-                      action="http://apps.nationalarchives.gov.uk/search/quick_search.aspx">
-                    <div id="mobile-search-outer-wrapper" style="display: none;">
-                        <div id="mobile-search-wrapper">
-                            <input type="text" aria-required="true" required="" name="search_text"
-                                   placeholder="Search our website..." id="mobile-search-field" autocomplete="off">
-                            <input type="submit" id="mobileSearchSubmit" value="">
-                        </div>
-                        <div id="change-destination">
-                            <span><input type="radio" data-fieldname="search_text"
-                                         data-placeholder="Search our website..."
-                                         data-target="http://apps.nationalarchives.gov.uk/search/quick_search.aspx"
-                                         id="websiteSearch" name="searchDestination"
-                                         class="mobileSearchDestinationOption" checked=""><label for="websiteSearch">Search
-                                    our website</label></span>
-                            <span><input type="radio" data-fieldname="_q" data-placeholder="Search our records..."
-                                         data-target="/results/r" id="catalogueSearch" name="searchDestination"
-                                         class="mobileSearchDestinationOption"><label for="catalogueSearch">Search our
-                                    records</label></span>
-                        </div>
-                    </div>
-                </form>
-                <span id="search-expander" style="position: absolute; right: 5px;">&nbsp;</span></div>
-        </div>
-
-    </div>
-    <!-- This is where the mega-menu would be included -->
-    <div id="account-controls-wrapper">
-        <div id="account-controls" class="container">
-            <ul>
-                <li id="signin"><a href="http://discovery.nationalarchives.gov.uk/sign-in">Sign in</a></li>
-                <li id="register"><a href="http://discovery.nationalarchives.gov.uk/register">Register</a></li>
-                <li id="miniBasket">
-                <span class="noItems">
-                    <img src="images/basket.png" id="basketIcon" alt="Basket icon">
-                    No items
-                </span>
-                </li>
-
-            </ul>
+            </div>
         </div>
     </div>
+        <!-- This is where the mega-menu would be included -->
+        <div id="account-controls-wrapper">
+            <div id="account-controls" class="container">
+                <ul>
+                    <li class="currently-selected"><a href="">Dashboard</a></li>
+                    <li><a href="">Admin tools</a></li>
+                    <li><a href="">Staff audit</a></li>
+                    <li><a href="">Create page check</a></li>
+                    <li><a href="">Printing</a></li>
+                    <li><a href="">Reports</a></li>
+                    <li><a href="">Refunds</a></li>
+                    <li><a href="">Postage</a></li>
+                </ul>
+            </div>
+        </div>
 </header>
