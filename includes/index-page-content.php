61635ce7e964d4d754e31c7e0865034d1ec637f4
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-27 16:39:31 +0000

    Extracting the 'page count dashboard' to an include

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -15,62 +15,7 @@
                     <option value="">Monday 2nd February 2015</option>
                 </select>
             </form>
-
-            <div class="dashboard-component">
-                <div class="responsive-table">
-                    <table>
-                        <caption class="visually-hidden">Page count dashboard</caption>
-                        <tr>
-                            <td></td>
-                            <th scope="col" data-longform="Overdue" data-shortform="O/D"><span>Overdue</span></th>
-                            <th scope="col" data-longform="Monday" data-shortform="Mon"><span>Monday</span></th>
-                            <th scope="col" data-longform="Tuesday" data-shortform="Tue"><span>Tuesday</span></th>
-                            <th scope="col" data-longform="Wednesday" data-shortform="Wed"><span>Wednesday</span></th>
-                            <th scope="col" data-longform="Thursday" data-shortform="Thu"><span>Thursday</span></th>
-                            <th scope="col" data-longform="Friday" data-shortform="Fri"><span>Friday</span></th>
-                            <th scope="col" data-longform="Saturday" data-shortform="Sat"><span>Saturday</span></th>
-                            <th scope="col" data-longform="Sunday" data-shortform="Sun"><span>Sunday</span></th>
-                            <th scope="col" data-longform="Total" data-shortform="Total"><span>Total</span></th>
-                        </tr>
-                        <tr>
-                            <th scope="row">Page checks</th>
-                            <td><a href="">4</a></td>
-                            <td><a href="">1</a></td>
-                            <td><a href="">4</a></td>
-                            <td><a href="">3</a></td>
-                            <td><a href="">8</a></td>
-                            <td><a href="">5</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">28</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Certificates of natuaralisation</th>
-                            <td><a href="">25</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">2</a></td>
-                            <td><a href="">3</a></td>
-                            <td><a href="">4</a></td>
-                            <td><a href="">5</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">7</a></td>
-                            <td><a href="">28</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Total</th>
-                            <td><a href="">25</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">2</a></td>
-                            <td><a href="">3</a></td>
-                            <td><a href="">4</a></td>
-                            <td><a href="">5</a></td>
-                            <td><a href="">6</a></td>
-                            <td><a href="">7</a></td>
-                            <td></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+            <?php require_once('includes/page-count-dashboard.php'); ?>
         </div>
     </div>
 </section>
