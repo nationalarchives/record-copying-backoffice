43296e2ae97ba2d6ef223a40c1024583cf8163a0
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-29 13:08:45 +0000

    Addition of report page and related includes

---
+++ includes/report-page-breadcrumb.php
@@ -0,0 +1,15 @@
+<section class="row" id="holds-breadcrumb">
+    <div class="col starts-at-full ends-at-half clr">
+        <nav id="breadcrumb">
+            <ul>
+                <li><a href="http://www.nationalarchives.gov.uk/">Home</a></li> Dashboard
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="col starts-at-full ends-at-half clr">
+        <?php
+        // To include Discovery-like page controls simply introduce them as nav#page-controls > ul > li > a
+        ?>
+    </div>
+</section>
---
+++ includes/report-page-content.php
@@ -0,0 +1,10 @@
+<section class="row">
+    <div class="col starts-at-full clr holding-box">
+        <div class="heading-holding-banner">
+            <h1><span><span>Dashboard</span></span></h1>
+        </div>
+        <div class="breather">
+
+        </div>
+    </div>
+</section>
