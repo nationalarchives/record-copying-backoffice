a1ccd1973a9987eb2f2617edac017347aa86ca2d
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-02-03 16:08:45 +0000

    New layout for search form on larger screens. Now in two columns.

--- includes/search-form.php
+++ includes/search-form.php
@@ -7,10 +7,16 @@
                 <span><span>Search</span></span>
             </div>
             <div class="breather">
-                <div class="field-row">
-                    <label for="copying-search">Search for a page check or copying order</label><input type="text" class="width-half"/>
+                <div class="grid-within-grid-two-item">
+                    <div class="field-row">
+                        <label for="copying-search">Search for a page check or copying order</label><input type="text" class="width-half"/>
+                    </div>
+                    <div>
+
+                        <span class="emphasis-block">Tip: You can search for any part of a name, email address or postcode, or for an exact reference, transaction or reader ticket number</span>
+                    </div>
                 </div>
-                <span class="emphasis-block">Tip: You can search for any part of a name, email address or postcode, or for an exact reference, transaction or reader ticket number</span>
+
             </div>
         </fieldset>
     </form>
