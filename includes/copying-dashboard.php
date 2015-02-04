39edc33ce74a61a7de9495683a7081230df2b93e
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-02-04 12:44:22 +0000

    Reverting table style in response to business decision

--- includes/copying-dashboard.php
+++ includes/copying-dashboard.php
@@ -5,13 +5,13 @@
             <tr>
                 <td></td>
                 <th scope="col" data-longform="Overdue" data-shortform="O/D"><span>Overdue</span></th>
-                <th scope="col" data-longform="Today" data-shortform="Today"><span>Today</span></th>
-                <th scope="col" data-longform="Tomorrow" data-shortform="Tom."><span>Tomorrow</span></th>
-                <th scope="col" data-longform="Sunday" data-shortform="Sun"><span>Sunday</span></th>
                 <th scope="col" data-longform="Monday" data-shortform="Mon"><span>Monday</span></th>
                 <th scope="col" data-longform="Tuesday" data-shortform="Tue"><span>Tuesday</span></th>
                 <th scope="col" data-longform="Wednesday" data-shortform="Wed"><span>Wednesday</span></th>
                 <th scope="col" data-longform="Thursday" data-shortform="Thu"><span>Thursday</span></th>
+                <th scope="col" data-longform="Friday" data-shortform="Fri"><span>Friday</span></th>
+                <th scope="col" data-longform="Saturday" data-shortform="Sat"><span>Saturday</span></th>
+                <th scope="col" data-longform="Sunday" data-shortform="Sun"><span>Sunday</span></th>
                 <th scope="col" data-longform="Total" data-shortform="Total"><span>Total</span></th>
             </tr>
             <tr>
@@ -75,8 +75,5 @@
                 <td><a href="">157</a></td>
             </tr>
         </table>
-        <div class="button-holder">
-            <a class="discoveryPrimaryCallToActionLink" href="">Previous week</a> <a class="discoveryPrimaryCallToActionLink" href="">Next week</a>
-        </div>
     </div>
 </div>
--- includes/page-check-dashboard.php
+++ includes/page-check-dashboard.php
@@ -5,21 +5,21 @@
             <tr>
                 <td></td>
                 <th scope="col" data-longform="Overdue" data-shortform="O/D"><span>Overdue</span></th>
-                <th scope="col" data-longform="Today" data-shortform="Today"><span>Today</span></th>
-                <th scope="col" data-longform="Tomorrow" data-shortform="Tom."><span>Tomorrow</span></th>
-                <th scope="col" data-longform="Sunday" data-shortform="Sun"><span>Sunday</span></th>
                 <th scope="col" data-longform="Monday" data-shortform="Mon"><span>Monday</span></th>
                 <th scope="col" data-longform="Tuesday" data-shortform="Tue"><span>Tuesday</span></th>
                 <th scope="col" data-longform="Wednesday" data-shortform="Wed"><span>Wednesday</span></th>
                 <th scope="col" data-longform="Thursday" data-shortform="Thu"><span>Thursday</span></th>
+                <th scope="col" data-longform="Friday" data-shortform="Fri"><span>Friday</span></th>
+                <th scope="col" data-longform="Saturday" data-shortform="Sat"><span>Saturday</span></th>
+                <th scope="col" data-longform="Sunday" data-shortform="Sun"><span>Sunday</span></th>
                 <th scope="col" data-longform="Total" data-shortform="Total"><span>Total</span></th>
             </tr>
             <tr>
                 <th scope="row">Page checks</th>
                 <td><a href="">4</a></td>
-                <td><a href="">6</a></td>
-                <td><a href="">0</a></td>
-                <td><a href="">0</a></td>
+                <td><a href="">1</a></td>
+                <td><a href="">4</a></td>
+                <td><a href="">3</a></td>
                 <td>0</td>
                 <td><a href="">5</a></td>
                 <td><a href="">6</a></td>
@@ -30,8 +30,8 @@
                 <th scope="row" data-longform="Certificates of naturalisation" data-shortform="Cert. Nat"><span>Certificates of natuaralisation</span></th>
                 <td><a href="">25</a></td>
                 <td><a href="">6</a></td>
-                <td><a href="">0</a></td>
-                <td><a href="">0</a></td>
+                <td><a href="">2</a></td>
+                <td><a href="">3</a></td>
                 <td><a href="">4</a></td>
                 <td><a href="">5</a></td>
                 <td>0</td>
@@ -41,13 +41,13 @@
             <tr>
                 <th scope="row">Total</th>
                 <td><a href="">25</a></td>
-                <td><a href="">12</a></td>
-                <td><a href="">0</a></td>
-                <td><a href="">0</a></td>
-                <td><a href="">4</a></td>
-                <td><a href="">10</a></td>
                 <td><a href="">6</a></td>
-                <td><a href="">13</a></td>
+                <td><a href="">2</a></td>
+                <td><a href="">3</a></td>
+                <td><a href="">4</a></td>
+                <td><a href="">5</a></td>
+                <td><a href="">6</a></td>
+                <td><a href="">7</a></td>
                 <td><a href="">142</a></td>
             </tr>
         </table>
