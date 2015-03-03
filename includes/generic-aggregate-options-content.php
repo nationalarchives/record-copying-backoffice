3856bf763c6d98d4f9ee3a6b668b74943a4e754b
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-03-03 13:12:14 +0000

    Initial work on aggregate options page

---
+++ includes/generic-aggregate-options-content.php
@@ -0,0 +1,70 @@
+<section class="row row-tabbed-content">
+    <div class="col ends-at-one-third clr box" id="search-filters">
+        <ul class="available-filters">
+            <li>
+                <h3><a href="#" class="filter-toggler expanded">Status</a></h3>
+                <div class="filter-togglee">
+                    <ul>
+                        <li>
+                            <input type="checkbox">
+                            <label>
+                                Pending page check
+                                <span class="filterNumber">(1,942)</span>
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>
+                                Page check in progress
+                                <span class="filterNumber">(3,887)</span>
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>
+                                Copying order pending
+                                <span class="filterNumber">(5,826)</span>
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>
+                                Order being copied
+                                <span class="filterNumber">(12,888)</span>
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>
+                                Complete and dispatched
+                                <span class="filterNumber">(16,845)</span>
+                            </label>
+                        </li>
+                    </ul><input name="Refine dates" value="Refine" type="submit" alt="Refine dates" title="Refine dates">
+                </div>
+            </li>
+            <li>
+                <h3><a href="#" class="filter-toggler expanded">Due date</a></h3>
+                <div class="filter-togglee">
+                    <ul>
+                        <li>
+                            <label class="inline" for="week-selection">Week commencing: </label>
+                            <select name="week-selection" id="week-selection">
+                                <option value="">Monday 19th January 2015</option>
+                                <option value="" selected="">Monday 26th January 2015</option>
+                                <option value="">Monday 2nd February 2015</option>
+                            </select>
+                        </li>
+                        <li></li>
+                    </ul>
+
+                    <input name="Refine dates" value="Refine" type="submit" alt="Refine dates" title="Refine dates">
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="col ends-at-two-thirds clr box">
+
+    </div>
+</div>
+</section>
