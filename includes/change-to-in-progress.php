1bc6faba0ea7523c3ee7f61538074092dcda6211
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-03-12 16:17:17 +0000

    Addition of link allowing staff to abandon a request immediately

--- includes/change-to-in-progress.php
+++ includes/change-to-in-progress.php
@@ -2,6 +2,7 @@
     <form method="get">
         <input type="hidden" name="status" value="in-progress">
         <div class="button-holder">
+        	<a href="abandon.php" class="abandon-link">Abandon page check</a>
             <input type="submit" value="Change status to 'in progress'">
         </div>
     </form>
