1a16685e4ecf134510b01ce6b4b91c5145c1e2ea
Chris Bishop <domingobishop@gmail.com>
2015-05-26 17:09:05 +0100

    Added target date revision history

---
+++ includes/revised-target-date-history-content.php
@@ -0,0 +1,32 @@
+<section class="row new-page">
+    <div class="col starts-at-full clr holding-box">
+        <div class="heading-holding-banner">
+	  <h1><span><span>Revision history for <?php echo REFERENCE_NUMBER; ?></span></span></h1>
+        </div>
+        <div class="breather">
+	  		<h3>The current target date for <?php echo REFERENCE_NUMBER; ?> is <strong>26/4/2014</strong></h3>
+	  		<ul class="target-date-history-results">
+	  			<li>
+	  				<h3><strike>12/4/2014</strike> target date revised to 26/4/2014</a></h3>
+	  				<p><strong>Reason for target date change:</strong> Erat dicat luptatum ad ius, vel quando dicunt ea. Ut mentitum assentior per.</p>
+	  			</li>
+	  			<li>
+	  				<h3><strike>06/4/2014</strike> target date revised to 12/4/2014</a></h3>
+	  				<p><strong>Reason for target date change:</strong> Eros solum nobis cum ad. Aliquip adipiscing ad mel, ius vidit rebum mollis ex.</p>
+	  			</li>
+	  			<li>
+	  				<h3><strike>31/3/2014</strike> target date revised to 06/4/2014</a></h3>
+	  				<p><strong>Reason for target date change:</strong> Postea ancillae scriptorem mei ad, mel no senserit omittantur, option bonorum nostrum sea an.</p>
+	  			</li>
+	  		</ul>
+        </div>
+</section>
+<div class="holds-columns-together">
+		    <form action="details.php" method="get">
+		        <input type="hidden" name="status" value="in-progress">
+		        <div class="button-holder">
+		            <input type="submit" value="Return to Order details">
+		        </div>
+		    </form>
+</div>
+
