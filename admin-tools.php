fee1b777db04ffed568e74762597ef92a15730a7
Chris Bishop <domingobishop@gmail.com>
2015-08-04 14:19:49 +0100

    Added admin tools page

---
+++ admin-tools.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-gb" class="no-js">
+<?php require_once('includes/head.php'); ?>
+<body class="admin-tools">
+<?php require_once('includes/header.php'); ?>
+<main id="page_wrap" class="container" role="main">
+    <?php require_once('includes/admin-tools-breadcrumb.php'); ?>
+    <?php require_once('includes/admin-tools-content.php'); ?>
+</main>
+<?php require_once('includes/footer.php'); ?>
+<?php require_once('includes/footer-scripts.php'); ?>
+</body>
+</html>
