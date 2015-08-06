962312468c9c266fee5193b0144ae0905a412875
Chris Bishop <domingobishop@gmail.com>
2015-08-06 15:05:38 +0100

    suspended history added

---
+++ includes/suspended-order-history-breadcrumb.php
@@ -0,0 +1,18 @@
+<section class="row" id="holds-breadcrumb">
+    <div class="col starts-at-full ends-at-half clr">
+        <nav id="breadcrumb">
+	  <ul>
+	      <li><a href="http://www.nationalarchives.gov.uk/">Home</a><a href="">Reports</a></li>
+	      Reference number: <?php echo(REFERENCE_NUMBER); ?>
+	      </li>
+	  </ul>
+        </nav>
+    </div>
+    <div class="col starts-at-full ends-at-half clr">
+        <nav id="page-controls">
+	  <ul>
+	      <li><a href="">Print details</a></li>
+	  </ul>
+        </nav>
+    </div>
+</section>
